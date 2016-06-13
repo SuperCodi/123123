0fb4fdb344aca77f82feb5a7f48b2cccd2101b5b
lanou3g <946608155@qq.com>
2016-06-13 10:14:21 +0800

    是热水管色鬼

---
+++ 喜闻乐见/喜闻乐见/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  喜闻乐见
+//
+//  Created by 朱博文 on 16/6/12.
+//  Copyright © 2016年 boon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 喜闻乐见/喜闻乐见/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  喜闻乐见
+//
+//  Created by 朱博文 on 16/6/12.
+//  Copyright © 2016年 boon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
